295b7dd01e4f07418f9772908c0e82d6feed4324
Seibell <rryanlimcw@gmail.com>
2023-09-30 23:02:22 +0800

    added upload data for prediction page on frontend

--- backend/app.py
+++ backend/app.py
@@ -2,7 +2,7 @@ from flask import Flask, jsonify
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, origins="*", methods=['GET', 'POST', 'PUT', 'DELETE'])
 
 @app.route('/api/hello', methods=['GET'])
 def hello():
---
+++ client/src/pages/PredictionsPage/PredictionsPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import UploadForm from "./UploadForm";
+
+function PredictionsPage() {
+  return (
+    <div>
+      <UploadForm />
+    </div>
+  );
+}
+
+export default PredictionsPage;
