1f3aac61f7d8e8b87fac0f336f0d86eb0b5b0441
WangYuTengg <wyuteng123@gmail.com>
2023-09-30 21:05:33 +0800

    refactor

---
+++ client/src/components/Row1.jsx
@@ -0,0 +1,13 @@
+import DashboardBox from "./DashboardBox";
+
+const Row1 = () => {
+  return (
+    <>
+      <DashboardBox gridArea="a"></DashboardBox>
+      <DashboardBox gridArea="b"></DashboardBox>
+      <DashboardBox gridArea="c"></DashboardBox>
+    </>
+  );
+};
+
+export default Row1;
